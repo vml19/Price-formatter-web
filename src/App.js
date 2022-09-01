8b3bbfd93a07002c4a98a2e682a4917f102d7c2a
vml19 <vml19.t@gmail.com>
2022-09-01 10:45:24 +0530

    Fixes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 function App() {
+
+  React.useEffect(() => {
+    console.log('Calling hook ..');
+    loadCurrency(1); //Default --> USD
+  }, []);
 
   const options = [
     { value: '1', text: 'USD' },
@@ -8,35 +13,26 @@ function App() {
     { value: '3', text: 'GBP' }
   ]
 
-  const [selectedCcy, setSelectedCcy] = useState(options[0].value);
   const handleChange = event => {
-    console.log(event.target.value);
-    setSelectedCcy(event.target.value);
+    console.log('Calling handleChange, selectedCcy->' + event.target.value);
+    loadCurrency(event.target.value);
   };
 
   const [currency, setCurrency] = useState([]);
-  useEffect(() => {
-    fetch('api/currency/' + 1)
+
+  const loadCurrency = selectedCcy => {
+    let url = 'api/currency/' + selectedCcy;
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         const ccy: Currency = data;
         setCurrency(ccy);
-      })
-  }, []);
-
-  const getCurrency  => {
-    fetch('api/currency/' + 1)
-      .then(response => response.json())
-      .then(data => {
-        const ccy: Currency = data;
-        setCurrency(ccy);
-      })
-  };
-
+      });
+  }
   return (
     <div className="App" style={{ margin: 10 }}>
       <div style={{ width: 400, padding: 10 }}>
-        <select value={selectedCcy} onChange={handleChange}>
+        <select onChange={handleChange}>
           {options.map(option => (
             <option key={option.value} value={option.value}>
               {option.text}
@@ -47,31 +43,34 @@ function App() {
       <div style={{ width: 400, padding: 10 }}>
         <table style={{ backgroundColor: 'black', border: 0 }}>
           <tbody>
+
             <tr>
               <th colSpan={2} style={{ fontSize: 40, color: 'white', backgroundColor: 'black' }}>{currency.name}</th>
             </tr>
-           
-              <tr style={{ fontSize: 100 }}>
-                <td style={{ color: 'black', backgroundColor: 'green' }}>
-                  <div>
-                    <sub>{currency.bidPrice?.bigFigure}</sub>
-                    <span style={{ fontWeight: 'bold' }}>{currency.bidPrice?.dealingPrice}</span>
-                    <sup>{currency.bidPrice?.fractionalPips}</sup>
-                  </div>
-                </td>
-                <td style={{ color: 'black', backgroundColor: 'yellow' }}>
-                  <div>
-                    <sub>{currency.offerPrice?.bigFigure}</sub>
-                    <span style={{ fontWeight: 'bold' }}>{currency.offerPrice?.dealingPrice}</span>
-                    <sup>{currency.offerPrice?.fractionalPips}</sup>
-                  </div>
-                </td>
-              </tr>
-           
+
+            <tr style={{ fontSize: 40 }}>
+              <td style={{ color: 'black', backgroundColor: 'green' }}>
+                <div>
+                  <sub>{currency.bidPrice?.bigFigure}</sub>
+                  <span style={{ fontWeight: 'bold' }}>{currency.bidPrice?.dealingPrice}</span>
+                  <sup>{currency.bidPrice?.fractionalPips}</sup>
+                </div>
+              </td>
+              <td style={{ color: 'black', backgroundColor: 'yellow' }}>
+                <div>
+                  <sub>{currency.offerPrice?.bigFigure}</sub>
+                  <span style={{ fontWeight: 'bold' }}>{currency.offerPrice?.dealingPrice}</span>
+                  <sup>{currency.offerPrice?.fractionalPips}</sup>
+                </div>
+              </td>
+            </tr>
+
             <tr style={{ fontSize: 20, fontWeight: 'bold', color: 'black', backgroundColor: 'gray' }}>
               <td>Bid</td>
               <td>Offer</td>
-            </tr></tbody>
+            </tr>
+            
+            </tbody>
         </table>
       </div>
     </div >
